b8955d5669b26dcca81196c779ca46dd2a59ca12
fahadfarooq <fahadfarooq54@gmail.com>
2018-04-10 16:47:36 +0500

    new app

--- config/policies.js
+++ config/policies.js
@@ -2,55 +2,20 @@
  * Policy Mappings
  * (sails.config.policies)
  *
- * Policies are simple functions which run **before** your controllers.
- * You can apply one or more policies to a given controller, or protect
- * its actions individually.
- *
- * Any policy file (e.g. `api/policies/authenticated.js`) can be accessed
- * below by its filename, minus the extension, (e.g. "authenticated")
- *
- * For more information on how policies work, see:
- * http://sailsjs.org/#!/documentation/concepts/Policies
+ * Policies are simple functions which run **before** your actions.
  *
  * For more information on configuring policies, check out:
- * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.policies.html
+ * https://sailsjs.com/docs/concepts/policies
  */
-
 
 module.exports.policies = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Default policy for all controllers and actions (`true` allows public     *
-  * access)                                                                  *
-  *                                                                          *
-  ***************************************************************************/
+  '*': 'is-logged-in',
 
-  // '*': true,
+  // Bypass the `is-logged-in` policy for:
+  'entrance/*': true,
+  'account/logout': true,
+  'view-homepage-or-redirect': true,
+  'deliver-contact-form-message': true,
 
-  /***************************************************************************
-  *                                                                          *
-  * Here's an example of mapping some policies to run before a controller    *
-  * and its actions                                                          *
-  *                                                                          *
-  ***************************************************************************/
-	 CategoryController: {
-    create : 'isAdmin',
-   },
-   EducationLevelController: {
-    create : 'isAdmin',
-   },
-   JobController: {
-    create : 'isAdmin',
-   },
-   AdminController:{
-    teacher:'isAdmin',
-    teacherOne:'isAdmin',
-    home:'isAdmin',
-    master:'isAdmin'
-   },
-   PersonController:{
-     getOne:'isAuthenticated',
-     isPublished:'isAuthenticated'
-   }
 };
